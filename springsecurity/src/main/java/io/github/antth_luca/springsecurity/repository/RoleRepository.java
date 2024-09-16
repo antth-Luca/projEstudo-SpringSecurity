8188d0fff52113316bf72e2cd8f267a5e6865473
Luca Anthony <lucamafort05@gmail.com>
2024-09-16 09:58:52 -0300

    Remoção de projeto: vídeos desatualizados

--- springsecurity/src/main/java/io/github/antth_luca/springsecurity/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package io.github.antth_luca.springsecurity.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import io.github.antth_luca.springsecurity.entities.Role;
-
-@Repository
-public interface RoleRepository extends JpaRepository<Role, Long> { }
--- springsecurity/src/main/java/io/github/antth_luca/springsecurity/repository/TweetRepository.java
+++
@@ -1,9 +0,0 @@
-package io.github.antth_luca.springsecurity.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import io.github.antth_luca.springsecurity.entities.Tweet;
-
-@Repository
-public interface TweetRepository extends JpaRepository<Tweet, Long> { }
